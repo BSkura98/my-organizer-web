136eb540863c61d58525813208325f217036cfc0
Bartłomiej Skura <52361028+BSkura98@users.noreply.github.com>
2024-02-15 09:34:31 +0100

    Create Project model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Project } from '../shared/models/project';
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  items = [
+    new Project('Project 1'),
+    new Project('Project 2', "It's a second project"),
+    new Project('Project 3'),
+  ];
   title = 'my-organizer-web';
 }

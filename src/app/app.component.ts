58ad10befe6f30eed586f971870e00fdee329e56
Bartłomiej Skura <52361028+BSkura98@users.noreply.github.com>
2024-02-16 10:33:07 +0100

    Create simple view of projects (#1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgFor } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 import { Project } from '../shared/models/project';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor, MatCardModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
